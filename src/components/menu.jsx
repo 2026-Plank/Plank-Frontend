6d7a4b1318388e3d136621698fdc396edeffa907
ygunk <ygunkun1015@gmail.com>
2026-06-04 21:00:42 +0900

    menu roading test

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -104,6 +104,7 @@ const TabText = styled.span`
 `;
 
 export default function Menu() {
+    console.log("Menu Render");
     const navigate = useNavigate();
     const location = useLocation(); // ★ 선언문 추가
 
