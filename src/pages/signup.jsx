12a72ca0c3c7e8aa5aada4c4d56f52dc086b7e3e
ygunk <ygunkun1015@gmail.com>
2026-06-04 20:39:15 +0900

    api 주소 변경

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -176,7 +176,7 @@ export default function Signup() {
         }
 
         try {
-            const res = await fetch('/api/auth/signup', {
+            const res = await apiRequest('/api/auth/signup', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
